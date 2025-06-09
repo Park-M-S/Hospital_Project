f2c83956193bc49de4ab7c76d50b5565578ba4fe
Park-M-S <mmmg0306@g.shingu.ac.kr>
2025-06-09 15:05:29 +0900

    Delete project/frontend/src directory

--- project/frontend/src/App.vue
+++
@@ -1,8 +0,0 @@
-<!-- src/App.vue -->
-<template>
-    <router-view />
-</template>
-<script>
-import '@/styles/default.css'
-import '@/styles/font.css'
-</script>
--- project/frontend/src/services/utils/toggleSidebar.js
+++
@@ -1,6 +0,0 @@
-export default {
-  // 토글 사비드바 버튼
-  toggle_Sidebar() {
-    this.isSideBar = !this.isSideBar;
-  },
-}
