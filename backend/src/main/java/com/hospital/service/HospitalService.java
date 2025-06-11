63951984c38d064415b03237a12eacf5faf3e443
Park-M-S <mmmg0306@g.shingu.ac.kr>
2025-06-12 00:16:28 +0900

    Delete backend directory

--- backend/src/main/java/com/hospital/service/HospitalService.java
+++
@@ -1,9 +0,0 @@
-package com.hospital.service;
-
-import java.util.List;
-
-import com.hospital.dto.web.HospitalDTO;
-
-public interface HospitalService {
-    List<HospitalDTO> getHospitals(String sub, double userLat, double userLng, double radius, List<String> tags);
-}
