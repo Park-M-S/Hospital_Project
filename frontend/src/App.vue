1e58ed2bee1b29e79895e86b853f28cdcbd55020
Park-M-S <mmmg0306@g.shingu.ac.kr>
2025-06-14 15:10:17 +0900

    Update App.vue

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -152,6 +152,7 @@ import toggleSidebar from '@/services/utils/toggleSidebar.js';
 import toggleBottombar from '@/services/utils/toggleBottombar.js';
 import tag from '@/services/utils/tags.js';
 import selectDiagnosis from '@/services/utils/selectDiagnosis';
+import { setUserLocation } from '@/services/setUserLocation.js';
 
 
 export default {
