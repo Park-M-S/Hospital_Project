3ac1520ff04cefe8c41c45730eb9b83761b1bd74
Park-M-S <mmmg0306@g.shingu.ac.kr>
2025-06-14 16:43:22 +0900

    Update dataSource.js

--- frontend/src/services/api/data/dataSource.js
+++ frontend/src/services/api/data/dataSource.js
@@ -6,44 +6,49 @@ export default {
     this.pharmacyList = null;
     try {
       if (this.subs && this.subs.length != 0) {
-        const res = await axios.get('http://56.155.87.205//hospitalsData', {
+        const res = await axios.get('/api/hospitalsData', {  // 수정: 상대경로 사용
           params: {
-            subs: this.subs.join(','),        // 진료과 전체 전달
+            subs: this.subs,                    // 수정: 배열 그대로 전달
             userLat: this.$store.getters.userLat,
             userLng: this.$store.getters.userLng,
             radius: this.radius,
-            tags: this.subsTag.join(','),
+            tags: this.subsTag,                 // 수정: 배열 그대로 전달
           }
         });
+        
+        console.log('병원 API 응답:', res.data);
         this.hospitalList = res.data;
-        if (this.map) {
+        
+        if (this.map && Array.isArray(this.hospitalList)) {
           this.loadMaker();
         }
       }
     } catch (err) {
-      console.error('에러 발생 : ', err);
+      console.error('병원 데이터 에러:', err);
     }
   },
-
+  
   // 약국 데이터 가져오기
   async fetch_pharmacy() {
     try {
       if (this.subsTag && this.subsTag.length != 0) {
-        const res = await axios.get('http://localhost:8889/hospital_main/pharmaciesData', {
+        const res = await axios.get('/api/pharmaciesData', {  // 수정: 상대경로 사용
           params: {
             userLat: this.$store.getters.userLat,
             userLng: this.$store.getters.userLng,
             radius: this.radius,
           }
         });
+        
+        console.log('약국 API 응답:', res.data);
         this.pharmacyList = res.data;
-        // this.pharmacyList.forEach(p => alert(p.pharmacyName));
-        if (this.map) {
+        
+        if (this.map && Array.isArray(this.pharmacyList)) {
           this.loadMaker();
         }
       }
     } catch (err) {
-      console.error('에러 발생 : ', err);
+      console.error('약국 데이터 에러:', err);
     }
   }
 }
