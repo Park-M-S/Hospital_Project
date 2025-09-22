b2413e06487e920270e2e0c95abd6e0537b67015
YOUHEETAE <dbgmlxo01@g.shingu.ac.kr>
2025-09-22 22:24:47 +0900

    테스트 브랜치 병합

--- hospital_main/src/main/java/com/hospital/entity/HospitalMain.java
+++ hospital_main/src/main/java/com/hospital/entity/HospitalMain.java
@@ -4,19 +4,20 @@ import java.util.Set;
 
 import org.hibernate.annotations.DynamicUpdate;
 
-import jakarta.persistence.CascadeType;
 // JPA 관련 임포트 추가
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Lob;
 import jakarta.persistence.NamedAttributeNode;
 import jakarta.persistence.NamedEntityGraph;
 import jakarta.persistence.NamedEntityGraphs;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
-
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -42,9 +43,11 @@ import lombok.ToString;
 				@NamedAttributeNode("medicalSubjects"), @NamedAttributeNode("proDocs") }) })
 public class HospitalMain {
 
+
+	
 	@Id
 	@Column(name = "hospital_code", nullable = false, length = 255) // 컬럼 이름과 속성 지정
-	private String hospitalCode; // PK
+	private String hospitalCode;
 
 	@Column(name = "hospital_name", nullable = false, length = 255)
 	private String hospitalName;
@@ -64,14 +67,20 @@ public class HospitalMain {
 	@Column(name = "hospital_homepage", length = 255)
 	private String hospitalHomepage;
 
-	@Column(name = "doctor_num")
-	private Integer doctorNum;
+	@Column(name = "total_doctors")
+	private String totalDoctors;
 
 	@Column(name = "coordinate_x")
 	private Double coordinateX;
 
 	@Column(name = "coordinate_y")
 	private Double coordinateY;
+	
+    @Lob
+    @Column(name = "subject_names", length = 1000)
+    private String subjectNames; // ["내과", "외과", "정형외과"]
+    
+    
 
 	@OneToOne(mappedBy = "hospital",
 
